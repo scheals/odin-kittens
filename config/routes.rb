dc27488594b1074801d327100aa367318ec11ee4
Scheals <95084627+scheals@users.noreply.github.com>
2023-01-17 23:11:41 +0100

    update: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'kittens/new'
-  get 'kittens/create'
-  get 'kittens/edit'
-  get 'kittens/update'
-  get 'kittens/delete'
-  get 'kittens/show'
-  get 'kittens/index'
+  resources :kittens
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'kittens#index'
 end
