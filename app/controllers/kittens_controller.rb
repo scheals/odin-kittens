89dd36de4ed3c3a4b6a958d1fcbcd8779a448963
Scheals <95084627+scheals@users.noreply.github.com>
2023-01-17 23:57:25 +0100

    feat: add submitting kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,8 +1,17 @@
 class KittensController < ApplicationController
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:success] = "#{@kitten.name} added to the Kitten Database!"
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -20,5 +29,11 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
